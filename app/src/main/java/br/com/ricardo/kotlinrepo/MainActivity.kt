1d3bbc3f0b4c198f75f2bfd7907b773d7a2bcc20
rickale <ti.ricardo.sousa@gmail.com>
2019-07-04 00:43:06 -0300

    Creating functions.

--- app/src/main/java/br/com/ricardo/kotlinrepo/MainActivity.kt
+++ app/src/main/java/br/com/ricardo/kotlinrepo/MainActivity.kt
@@ -10,15 +10,29 @@ class MainActivity : AppCompatActivity() {
     val complemento = "Show!"
     val idade = 10
 
+    val a = 10
+    val b = 20
+    val c = 30
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        teste()
+        calculaBonus(5, 6, 7)
+
+        hello("Ricardo")
 
     }
 
     fun teste(){
-        text_name.text = frase + "${complemento.length} $complemento"
+        text_name.text = frase + "${complemento.length} $idade"
+    }
+
+    fun calculaBonus(a: Int, b: Int, c: Int){
+        text_name.text = "A função é: ${a + b * c}"
+    }
+
+    fun hello(nome: String){
+        text_age.text = nome
     }
 }
